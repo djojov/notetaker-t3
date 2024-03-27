deb3934a96c5c488e2cfbfb6cdd55d41f381ff2c
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-27 18:23:17 +0100

    Remove Post model and related code

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { noteRouter } from "./routers/note";
 import { topicRouter } from "./routers/topic";
@@ -9,7 +8,6 @@ import { topicRouter } from "./routers/topic";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   topic: topicRouter,
   note: noteRouter,
 });
