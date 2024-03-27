5cb8197621db98004cfde9831ed15712935dd728
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-27 18:18:31 +0100

    Add NoteCard and NoteEditor components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import { useSession } from "next-auth/react";
 import Head from "next/head";
 import React, { useState } from "react";
 import Header from "~/components/Header";
+import { NoteCard } from "~/components/NoteCard";
+import { NoteEditor } from "~/components/NoteEditor";
 
 import { api, type RouterOutputs } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -34,8 +34,9 @@ const Content: React.FC = () => {
     undefined,
     {
       enabled: sessionData?.user !== undefined,
-      onSuccess: (data) => {
-        setSelectedTopic(slectedTopic ?? data[0] ?? null);
+
+      onSuccess: () => {
+        setSelectedTopic(selectedTopic ?? data[0] ?? null);
       },
     },
   );
@@ -43,6 +44,27 @@ const Content: React.FC = () => {
   const createTopic = api.topic.create.useMutation({
     onSuccess: () => {
       void refreshTopics();
+    },
+  });
+
+  const { data: notes, refetch: refreshNotes } = api.note.getAll.useQuery(
+    {
+      topicId: selectedTopic?.id ?? "",
+    },
+    {
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    },
+  );
+
+  const createNote = api.note.create.useMutation({
+    onSuccess: () => {
+      void refreshNotes();
+    },
+  });
+
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refreshNotes();
     },
   });
 
@@ -79,7 +101,27 @@ const Content: React.FC = () => {
           }}
         />
       </div>
-      <div className="col-span-3"></div>
+      <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+        <NoteEditor
+          onSave={({ title, content }) => {
+            void createNote.mutate({
+              title,
+              content,
+              topicId: selectedTopic?.id ?? "",
+            });
+          }}
+        />
+      </div>
     </div>
   );
 };
