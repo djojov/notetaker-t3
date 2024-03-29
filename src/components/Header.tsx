e863d0d3b6fdf5b7caed2433d3bf6e697538aa23
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-03-29 23:33:10 +0100

    Add theme provider and theme toggle components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,32 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { Mail } from "lucide-react";
+import {
+  GitBranch,
+  Github,
+  GithubIcon,
+  Instagram,
+  LucideGithub,
+  Mail,
+} from "lucide-react";
+import { ModeToggle } from "./ui/theme-toggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "~/lib/utils";
+import Link from "next/link";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 function Header() {
   const { data: sessionData } = useSession();
@@ -9,24 +34,61 @@ function Header() {
   return (
     <div className="border-border/40 sticky top-0 border-b p-4 backdrop-blur">
       <div className="container flex flex-row items-center justify-between">
-        <div className="text-xl font-bold text-zinc-50">
-          {sessionData?.user?.name
-            ? `Notes for ${sessionData.user.name}`
-            : "Notetaker T3"}
-        </div>
-        {sessionData?.user ? (
-          <Avatar
-            onClick={() => void signOut()}
-            className="cursor-pointer rounded-full"
-          >
-            <AvatarImage src={sessionData?.user?.image ?? ""} />
-            <AvatarFallback>{sessionData?.user?.name?.[0]}</AvatarFallback>
-          </Avatar>
-        ) : (
-          <Button onClick={() => void signIn()}>
-            <Mail className="mr-2 h-4 w-4" /> Sign in with Google
+        <div className="text-xl font-bold text-primary">Notetaker T3</div>
+        <div className="flex gap-x-4">
+          {sessionData?.user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="cursor-pointer rounded-full">
+                  <AvatarImage src={sessionData?.user?.image ?? ""} />
+                  <AvatarFallback>
+                    {sessionData?.user?.name?.[0]}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel
+                  className={cn(
+                    "font-sans font-normal antialiased",
+                    fontSans.variable,
+                  )}
+                >
+                  <div className="flex flex-col space-y-1">
+                    <p className="text-sm font-medium leading-none">
+                      {sessionData?.user?.name}
+                    </p>
+                    <p className="text-muted-foreground text-xs leading-none">
+                      {sessionData?.user?.email}
+                    </p>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onClick={() => void signOut()}
+                  className={cn(
+                    "cursor-pointer  font-sans antialiased",
+                    fontSans.variable,
+                  )}
+                >
+                  Sign out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Button onClick={() => void signIn()}>
+              <Mail className="mr-2 h-4 w-4" /> Sign in with Google
+            </Button>
+          )}
+          <Button variant="outline" size="icon">
+            <Link
+              href={"https://github.com/djojov/notetaker-t3"}
+              target="_blank"
+            >
+              <Github className="h-4 w-4" />
+            </Link>
           </Button>
-        )}
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ThemeProvider } from "~/components/theme-provider";
+import { cn } from "~/lib/utils";
 
-const inter = Inter({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
@@ -18,8 +20,17 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <main className={`font-sans ${inter.variable}`}>
-        <Component {...pageProps} />
+      <main
+        className={cn("bg-background font-sans antialiased", fontSans.variable)}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Component {...pageProps} />
+        </ThemeProvider>
       </main>
     </SessionProvider>
   );
