0f30e400c4166672066716060f85750ea4995dd7
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2024-04-01 20:05:56 +0200

    Change header to improve user experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { Github, Mail } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import {
@@ -22,49 +21,52 @@ function Header() {
         <a href="#" className="text-xl font-bold text-primary">
           Notetaker T3
         </a>
-        <div className="flex gap-x-4">
+        <div className="flex items-center gap-x-1">
           {sessionData?.user ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Avatar className="cursor-pointer rounded-full">
-                  <AvatarImage src={sessionData?.user?.image ?? ""} />
-                  <AvatarFallback>
-                    {sessionData?.user?.name?.[0]}
-                  </AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">
-                      {sessionData?.user?.name}
-                    </p>
-                    <p className="text-xs leading-none text-muted-foreground">
-                      {sessionData?.user?.email}
-                    </p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  onClick={() => void signOut()}
-                  className="cursor-pointer text-red-600"
-                >
-                  Sign out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <div className="mr-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="h-9 w-9 cursor-pointer rounded-full">
+                    <AvatarImage src={sessionData?.user?.image ?? ""} />
+                    <AvatarFallback>
+                      {sessionData?.user?.name?.[0]}
+                    </AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>
+                    <div className="flex flex-col space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        {sessionData?.user?.name}
+                      </p>
+                      <p className="text-xs leading-none text-muted-foreground">
+                        {sessionData?.user?.email}
+                      </p>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() => void signOut()}
+                    className="cursor-pointer text-red-600"
+                  >
+                    Sign out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           ) : (
             <Button onClick={() => void signIn()}>
               <Mail className="mr-2 h-4 w-4" /> Sign in with Google
             </Button>
           )}
-          <Button variant="outline" size="icon">
-            <Link
-              href={"https://github.com/djojov/notetaker-t3"}
-              target="_blank"
-            >
-              <Github className="h-4 w-4" />
-            </Link>
+          <Button
+            onClick={() =>
+              window.open("https://github.com/djojov/notetaker-t3", "_blank")
+            }
+            variant="ghost"
+            size="icon"
+          >
+            <Github className="h-4 w-4" />
           </Button>
           <ModeToggle />
         </div>
